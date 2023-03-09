b1eb1701fc422a183ce8374aa04c38622275e9a8
Crxshxr <103609586+Crxshxr@users.noreply.github.com>
2023-03-08 23:46:20 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [enteredEmail, setEnteredEmail] = useState();
+  const [inputIsInvalid, setInputIsInvalid] = useState(false);
+
+  function emailChangeHandler(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const emailIsValid = enteredEmail.includes('@'); 
+    setInputIsInvalid(!emailIsValid);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form onSubmit={submitFormHandler}>
+        <label htmlFor="email">Your email</label>
+        <input type="text" id="email" onChange={emailChangeHandler} />
+        <button>Submit</button>
+      </form>
+      {inputIsInvalid && <p>Invalid email address entered!</p>}
+    </section>
   );
 }
 
